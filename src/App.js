a0194a972a50736761bc22b0134c217d928d5e82
Floria Gu <gfzting@gmail.com>
2023-01-21 13:03:20 -0800

    Add D3

--- src/App.js
+++ src/App.js
@@ -2,57 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import * as d3 from 'd3';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div id="app">
+      <BarChart />
     </div>
   );
+}
+
+
+class BarChart extends React.Component {
+  componentDidMount() {
+    this.drawChart();
+  }
+
+  drawChart() {
+    const data = [12, 5, 6, 6, 9, 10];
+
+    const svg = d3.select("#app")
+      .append("svg")
+      .attr("width", 700)
+      .attr("height", 300);
+
+    svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", (d, i) => 300 - 10 * d)
+      .attr("width", 65)
+      .attr("height", (d, i) => d * 10)
+      .attr("fill", "green");
+  }
+
+  render() {
+    return null;
+  }
 }
 
 export default App;
