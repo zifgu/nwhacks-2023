f6b534ee936f431da38dcadb3cf7c02a98d3d4af
Floria Gu <gfzting@gmail.com>
2023-01-21 18:09:14 -0800

    Add rects behind text

--- src/components/Visualization.js
+++ src/components/Visualization.js
@@ -23,6 +23,7 @@ function useD3(renderChartFn, dependencies) {
 function Chart2({ data, width, numLifeStages }) {
   const center = [width / 2, width / 2];
   const radiusStep = width / (numLifeStages * 2);
+  const textOffsetRadius = radiusStep / 3;
 
   const ref = useD3(
     (chart) => {
@@ -40,6 +41,19 @@ function Chart2({ data, width, numLifeStages }) {
         return result;
       }
 
+      function getTextAngle(d, index) {
+        const itemsInLifeStage = itemsInLifeStages[d.lifeStage];
+        return index * (2 * Math.PI) / itemsInLifeStage;
+      }
+
+      function getTextTransform(d, index) {
+        const angle = getTextAngle(d, index);
+        const r = (d.lifeStage + 1) * radiusStep - textOffsetRadius;
+        const x = r * Math.cos(angle);
+        const y = r * Math.sin(angle);
+        return [x, y];
+      }
+
       const groupedData = d3.groups(data, d => d.lifeStage);
       const itemsInLifeStages = countPerLifeStage(data);
 
@@ -47,34 +61,85 @@ function Chart2({ data, width, numLifeStages }) {
         .domain([0, numLifeStages])
         .range(["lightgreen", "darkgreen"]);
 
+      // Rings
       const arc = d3.arc()
-        .startAngle((d, index) => index * (2 * Math.PI) / itemsInLifeStages[d.lifeStage])
-        .endAngle((d, index) => (index + 1) * (2 * Math.PI) / itemsInLifeStages[d.lifeStage])
-        .padAngle(0)
-        .innerRadius((d) => d.lifeStage * radiusStep)
-        .outerRadius((d) => (d.lifeStage + 1) * radiusStep);
+        .startAngle(0)
+        .endAngle(2 * Math.PI)
+        .innerRadius((d) => d[0] * radiusStep)
+        .outerRadius((d) => (d[0] + 1) * radiusStep);
 
-      const groups = chart.selectAll("g")
+      const rings = chart.selectAll("path")
+        .data(groupedData, d => d[0]);
+
+      const ringsEnter = rings
+        .enter()
+        .append("path");
+
+      ringsEnter.merge(rings)
+        .attr("d", arc)
+        .attr("fill", (d) => {
+          return colourScale(d[0]);
+        });
+
+      rings.exit().remove();
+
+      // Group by life stage
+      const lifeStageGroups = chart.selectAll("g")
         .data(groupedData)
         .join("g");
 
-      const arcs = groups.selectAll("path")
-        .data(d => d[1]);
-      const arcsEnter = arcs
-        .enter()
-        .append("path");
-      arcsEnter.merge(arcs)
-        .attr("d", arc)
-        .attr("fill", (d) => colourScale(d.lifeStage));
+      // Rects
+      const textRects = lifeStageGroups.selectAll("rect")
+        .data(d => d[1], d => d.value);
 
-      const text = groups.selectAll("text")
-        .data(d => d[1]);
+      const textRectsEnter = textRects.enter()
+        .append("rect")
+        .style("fill", "lightpink");
+
+      // Text
+      const text = lifeStageGroups.selectAll("text")
+        .data(d => d[1], d => d.value);
+
       const textEnter = text.enter()
         .append("text");
+
       textEnter.merge(text)
         .text((d) => d.value)
-        .style("text-anchor", "middle")
-        .attr("transform", (d, index) => `translate(${arc.centroid(d, index)})`);
+        .each(function(d, index) {
+          // Save calculated attributes
+          d.bbox = this.getBBox();
+
+          const angle = getTextAngle(d, index);
+          d.anchor = (Math.PI / 2 <= angle && angle <= 3 * Math.PI / 2) ? "end" : "start";
+        })
+        .style("text-anchor", (d) => d.anchor)
+        .transition()
+        .duration(500)
+        .attr("transform", (d, index) => {
+          return `translate(${getTextTransform(d, index)})`;
+        });
+
+      text.exit().remove();
+
+      // Update rect size and position
+      const xMargin = 10;
+      const yMargin = 2.5;
+
+      textRects.merge(textRectsEnter)
+        .attr("width", d => d.bbox.width + 2 * xMargin)
+        .attr("height", d => d.bbox.height + 2 * yMargin)
+        .attr("rx", d => d.bbox.height / 2);
+
+      textRects.merge(textRectsEnter)
+        .transition()
+        .duration(500)
+        .attr("transform", (d, index) => {
+          const textTransform = getTextTransform(d, index);
+          return `translate(${[
+            textTransform[0] - xMargin - (d.anchor === "end" ? d.bbox.width : 0),
+            textTransform[1] - yMargin - d.bbox.height * 3 / 4,
+          ]})`;
+        });
     },
     [data]
   );
@@ -84,126 +149,4 @@ function Chart2({ data, width, numLifeStages }) {
       <g ref={ref} id="chart" transform={`translate(${center[0]}, ${center[1]})`} />
     </svg>
   )
-}
-
-class Chart extends React.Component {
-  componentDidMount() {
-    this.initChart();
-  }
-
-  constructor(props) {
-    super(props);
-  }
-
-  initChart() {
-    const data = this.props.data;
-
-    const itemsInLifeStages = this.countPerLifeStage(data);
-    const totalLifeStages = 4;
-
-    const groupedData = d3.groups(data, d => d.lifeStage);
-
-    const width = 1600;
-    const center = [width / 2, width / 2];
-    const radiusStep = width / (totalLifeStages * 2);
-
-    // Chart
-    const svg = d3.select(`#${this.props.id}`)
-      .append("svg")
-      .attr("width", width)
-      .attr("height", width);
-
-    const chart = svg.append("g")
-      .attr("transform", `translate(${center[0]}, ${center[1]})`);
-
-    // With arcs
-    const colourScale = d3.scaleLinear()
-      .domain([0, totalLifeStages])
-      .range(["lightgreen", "darkgreen"]);
-
-    const arc = d3.arc()
-      .startAngle((d, index) => index * (2 * Math.PI) / itemsInLifeStages[d.lifeStage])
-      .endAngle((d, index) => (index + 1) * (2 * Math.PI) / itemsInLifeStages[d.lifeStage])
-      .padAngle(0)
-      .innerRadius((d) => d.lifeStage * radiusStep)
-      .outerRadius((d) => (d.lifeStage + 1) * radiusStep);
-
-    const groups = chart.selectAll("g")
-      .data(groupedData)
-      .enter()
-      .append("g");
-
-    groups.selectAll("path")
-      .data(d => d[1])
-      .enter()
-      .append("path")
-      .attr("d", arc)
-      .attr("fill", (d) => colourScale(d.lifeStage));
-
-    groups.selectAll("text")
-      .data(d => d[1])
-      .enter()
-      .append("text")
-      .text((d) => d.value)
-      .style("text-anchor", "middle")
-      .attr("transform", (d, index) => `translate(${arc.centroid(d, index)})`);
-
-    // groups.selectAll("line")
-    //   .data(d => d[1])
-    //   .enter()
-    //   .append("line")
-    //   .attr("x1", 0)
-    //   .attr("y1", 0)
-    //   .attr("x2", (d, index) => arc.centroid(d, index)[0])
-    //   .attr("y2", (d, index) => arc.centroid(d, index)[1])
-    //   .attr("stroke", "black");
-
-    // // Circles
-    // let circleGroups = chart.selectAll("g")
-    //   .data(groupedData)
-    //   .enter()
-    //   .append("g");
-    //
-    // circleGroups.append('circle')
-    //   .attr("r", (d, index) => (index + 1) * radiusStep)
-    //   .style("fill", "rgba(255, 0, 0, 0.25)");
-    //
-    // // Items
-    // circleGroups.selectAll("text")
-    //   .data(d => d[1])
-    //   .enter()
-    //   .append("text")
-    //   .style("text-anchor", "middle")
-    //   .attr("transform", (d, index) => {
-    //     const itemsInLifeStage = itemsInLifeStages[d.lifeStage];
-    //     const r = (d.lifeStage + 1) * radiusStep; // TODO:
-    //     const angle = index * (2 * Math.PI) / itemsInLifeStage;  // TODO:
-    //     const x = r * Math.cos(angle);
-    //     const y = r * Math.sin(angle);
-    //     return `translate(${x}, ${y})`;
-    //   })
-    //   .text((d) => d.value);
-  }
-
-  updateChart() {
-
-  }
-
-  countPerLifeStage(data) {
-    const result = {};
-
-    for (let item of data) {
-      if (item.lifeStage in result) {
-        result[item.lifeStage] += 1;
-      } else {
-        result[item.lifeStage] = 1;
-      }
-    }
-
-    return result;
-  }
-
-  render() {
-    return null;
-  }
 }
